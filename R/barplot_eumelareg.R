88038f1a5c9265e20f5a6a184302246ce2184ad9
MBender1992 <mbender1992@web.de>
2021-03-31 12:31:40 +0200

    Update

--- R/barplot_eumelareg.R
+++ R/barplot_eumelareg.R
@@ -15,7 +15,8 @@
 #' @param errorbar.size Thickness of errorbar line.
 #' @param textsize Size of the axes text defined by the function \code{theme_eumelareg_barplot}
 #' @param dotplot.binwidth Indirectly regulates size of the dots. Relative to the plot size.
-#' @param brewer.palette Color palette from package \code{RColorBrewer} or custom color palette.
+#' @param brewer.palette Color palette from package \code{RColorBrewer}. Can only be specified if custom.palette is NULL.
+#' @param custom.palette Custom color palette used in \code{scale_fill_manual()}. Can only be specified if brewer.palette is NULL.
 #' @param ylab Label for y-axis.
 #' @param y.breaks Argument to define breaks on y-axis. Default is \code{breakFun}, which displays the breaks as
 #'                 a multiple of 5.
@@ -31,7 +32,7 @@
 
 barplot_eumelareg <- function(data,x,y,n.vjust = 0.05, bar.border.size = 1, bar.alpha = 0.5, bar.width = 0.5, y.breaks = breakFun,
                               errorbar.fun = "mean_sdl", errorbar.width = 0.3, errorbar.size = 1.3, textsize = 14,
-                              brewer.palette = "RdYlBu", ylab = "Value", dotplot.binwidth = 1){
+                              brewer.palette = NULL,custom.palette = NULL, ylab = "Value", dotplot.binwidth = 1){
 
   data <- data.table::data.table(data)
 
@@ -40,17 +41,25 @@ barplot_eumelareg <- function(data,x,y,n.vjust = 0.05, bar.border.size = 1, bar.
   lim <- max(data[[y]])* (1+2*n.vjust)
 
 
-  ggplot(data, aes_string(x, y, fill = x)) +
+  p <- ggplot(data, aes_string(x, y, fill = x)) +
     geom_bar(stat = "summary", fun = "mean", color = "black", size = bar.border.size,
              alpha = bar.alpha, width = bar.width)+
     geom_errorbar(stat = "summary", fun.data = errorbar.fun,
                   fun.args = list(mult = 1), width  = errorbar.width, size = errorbar.size) +
     geom_dotplot(binaxis = "y", stackdir = "center", binwidth = dotplot.binwidth)+ # oder geom_jitter als Alternative?
-    geom_text(data = n, aes(label = eval(parse(text=y)), y = n$y.pos),  vjust = -1)+
+    geom_text(data = n, aes(label = eval(parse(text=y)), y = y.pos),  vjust = -1)+
     theme_eumelareg_barplot(textsize = textsize) +
     scale_y_continuous(expand = c(0, 0),breaks = y.breaks,  limits  = c(0, lim))+
-    scale_fill_brewer(palette = brewer.palette) +
     ylab(ylab)
+
+  if(!is.null(brewer.palette) & is.null(custom.palette)) {
+    p + scale_fill_brewer(palette = brewer.palette)
+  } else if (!is.null(custom.palette) & is.null(brewer.palette)) {
+    p + scale_fill_manual(values = custom.palette)
+  } else  if (!is.null(brewer.palette) & !is.null(custom.palette)) {
+    stop("Please only specify one of the two arguments \"brewer palette\" and \"custom palette\" ")
+  } else {
+    p
+  }
+
 }
-
-
