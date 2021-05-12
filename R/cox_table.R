422411d65f5b640ccbf3e94221acfa1109d79e91
MBender1992 <mbender1992@web.de>
2021-05-12 11:34:17 +0200

    update
    - documented ne wfunctions and deleted deprecated cox table functions

--- R/cox_table.R
+++ R/cox_table.R
@@ -1,7 +1,7 @@
 #' Print results from univariate or multivariate cox regression
 #'
-#' This function combines the results from multiple calls of \code{univariate_cox_output} and prints the output
-#' as an htmlTable generated with the \code{htmlTable} package or the output of multivariate cox regression. 
+#' This function combines the results from multiple calls of \code{cox_output} and prints the output
+#' as an htmlTable generated with the \code{htmlTable} package or the output of multivariate cox regression.
 #' @param data data.frame or data.table containing survival data.
 #' @param time the time interval from start of observation until date of event (e.g. disease progression or death)
 #' or censoring.
@@ -11,11 +11,12 @@
 #' @param printHTML Logical value. If TRUE output is printed as htmlTable. Default is TRUE.
 #' @param univariate Logical value. If TRUE output of univariate cox regression is printed. Else output of multivariate
 #' cox regression is printed. Default is TRUE.
+#' @param ... additional arguments to be passed on to \code{cox_output}
 #' @export
 
 cox_table <- function(data, time, status, vars, footnote = NULL, printHTML = TRUE, univariate = TRUE,...){
   if (univariate == TRUE) {
-    tmp <- lapply(vars, cox_output, data = data, time = time, status = status)
+    tmp <- lapply(vars, cox_output, data = data, time = time, status = status, ...)
     tmp <- dplyr::bind_rows(tmp)
   } else {
     tmp <- cox_output(data = data, time = time, status = status, vars = vars,...)
